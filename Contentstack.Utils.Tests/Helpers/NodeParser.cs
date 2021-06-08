2a00ce7d6d4a783d6137ed784a2e27bf66359417
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2021-06-08 13:29:32 +0530

    :construction: Json to HTML
    - NodeType Enum added
    - MarkType Enum added
    - Node Model added
    - TextNode Model added
    - Node Converter added
    - Json to Node Parser Unit test class added

---
+++ Contentstack.Utils.Tests/Helpers/NodeParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Contentstack.Utils.Models;
+using Contentstack.Utils.Tests.Constants;
+using Newtonsoft.Json;
+
+namespace Contentstack.Utils.Tests.Helpers
+{
+    public class NodeParser
+    {
+        public static Node parse(string jsonNode)
+        {
+            JsonSerializerSettings SerializerSettings = new JsonSerializerSettings();
+            JsonSerializer Serializer = JsonSerializer.Create(SerializerSettings);
+
+            return JsonConvert.DeserializeObject<Node>(jsonNode, SerializerSettings);
+        }
+    }
+}
---
+++ Contentstack.Utils/Enums/NodeType.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Contentstack.Utils.Enums
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum NodeType
+    {
+        [EnumMember(Value = "doc")]
+        Document,
+        [EnumMember(Value = "p")]
+        Paragraph,
+
+        [EnumMember(Value = "a")]
+        Link,
+        [EnumMember(Value = "img")]
+        Image,
+        [EnumMember(Value = "embed")]
+        Embed,
+
+        [EnumMember(Value = "h1")]
+        Heading_1,
+        [EnumMember(Value = "h2")]
+        Heading_2,
+        [EnumMember(Value = "h3")]
+        Heading_3,
+        [EnumMember(Value = "h4")]
+        Heading_4,
+        [EnumMember(Value = "h5")]
+        Heading_5,
+        [EnumMember(Value = "h6")]
+        Heading_6,
+
+        [EnumMember(Value = "ol")]
+        OrderList,
+        [EnumMember(Value = "ul")]
+        UnOrderList,
+        [EnumMember(Value = "li")]
+        ListItem,
+
+        [EnumMember(Value = "dhroc")]
+        Hr,
+
+        [EnumMember(Value = "table")]
+        Table,
+        [EnumMember(Value = "thead")]
+        TableHeader,
+        [EnumMember(Value = "tbody")]
+        TableBody,
+        [EnumMember(Value = "tfoot")]
+        TableFooter,
+        [EnumMember(Value = "tr")]
+        TableRow,
+        [EnumMember(Value = "th")]
+        TableHead,
+        [EnumMember(Value = "td")]
+        TableData,
+
+        [EnumMember(Value = "blockquote")]
+        BlockQuote,
+        [EnumMember(Value = "code")]
+        Code,
+
+        [EnumMember(Value = "text")]
+        Text,
+        [EnumMember(Value = "reference")]
+        Reference,
+
+        Unknown
+    }
+}
