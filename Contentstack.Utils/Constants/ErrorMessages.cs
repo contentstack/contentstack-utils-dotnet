99260e10b8bf0c29c1bf24c92e3a45199c358830
reeshika-h <reeshika.hosmani@contentstack.com>
2025-11-25 15:38:20 +0530

    Centralize error messages in Constants/ErrorMessages.cs
    - Created new ErrorMessages.cs constants file for centralized error message management
    - Updated RTEJsonConverter.cs to use error message constant instead of hardcoded string
    - Added Constants folder to project file
    - Improves maintainability by keeping all error messages in one location

---
+++ Contentstack.Utils/Constants/ErrorMessages.cs
@@ -0,0 +1,11 @@
+namespace Contentstack.Utils.Constants
+{
+    /// <summary>
+    /// Centralized error messages for the Contentstack Utils library
+    /// </summary>
+    public static class ErrorMessages
+    {
+        public const string InvalidRteJson = "Invalid RTE JSON. Provide a valid JSON structure and try again.";
+    }
+}
+
